1d1856f1ed0c1f305838ccda6bbad10fe5112961
akramabdelmaksoud <114350400+akramabdelmaksoud@users.noreply.github.com>
2024-06-08 02:01:14 +0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contact_form";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $job = $_POST['job'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO contacts (name, job, email, message) VALUES ('$name', '$job', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
